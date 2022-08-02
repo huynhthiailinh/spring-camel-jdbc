00a311f409cfed9a88cd016679646c0a6e525201
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-08-02 08:07:30 +0700

    Initial commit

---
+++ src/main/java/com/javainuse/springcameljdbc/SpringCamelJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse.springcameljdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamelJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamelJdbcApplication.class, args);
+	}
+
+}
